c441066105a54094179ab27084b286c24b01b955
tundeLayan <tunderoxyafolayan@gmail.com>
2023-02-09 22:14:38 +0100

    package.json installation

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React from "react";
-import { AnimatePresence } from "framer-motion";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react'
+import { AnimatePresence } from 'framer-motion'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 //Pages
-import Home from "./pages/home";
-import Model from "./pages/model";
+import Home from './pages/home'
+import Model from './pages/model'
 //components
-import Header from "./components/header";
+import Header from './components/header'
 //Styles
-import "./App.scss";
+import './App.scss'
 
 function App() {
   const imageDetails = {
     width: 524,
     height: 650,
-  };
+  }
 
   return (
     <Router>
       <Header />
       <Route
         render={({ location }) => (
-          <AnimatePresence initial={false} exitBeforeEnter>
+          <AnimatePresence initial={true} exitBeforeEnter>
             <Switch location={location} key={location.pathname}>
               <Route
                 exact
-                path='/'
+                path="/"
                 render={() => <Home imageDetails={imageDetails} />}
               />
               <Route
                 exact
-                path='/model/:id'
+                path="/model/:id"
                 render={() => <Model imageDetails={imageDetails} />}
               />
             </Switch>
@@ -37,7 +37,7 @@ function App() {
         )}
       />
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
